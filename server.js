009d00b7530c857ce165a125498afefbc4429941
lonnalu <lonnalu@gmail.com>
2021-09-08 15:53:10 -0700

    Adds middleware for views and starting homepage view

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
-
 const app = express();
-
 require('./config/db.connection');
-
 const PORT = 4000;
+
 
 // SECTION Internal Modules
 const restaurantControllers = require('./controllers/restaurant_controllers');
 
+
 // SECTION Middleware 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+
+// Views
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 // Custom middleware logger
 function logger(req,res,next) {
@@ -20,9 +23,10 @@ function logger(req,res,next) {
 };
 app.use(logger);
 
+
 // SECTION Routes
 app.get('/', (req,res) => {
-  res.send('Cravings Homepage')
+  res.render('home');
 });
 
 app.use('/restaurants', restaurantControllers);
@@ -30,5 +34,6 @@ app.use('/restaurants', restaurantControllers);
 app.get('/*', (req, res) => {
   res.send('Uh oh error')
 });
+
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
