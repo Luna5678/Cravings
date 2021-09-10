615c06f5050f1a0a3945f91c5be4f380e0cf0e49
lonnalu <lonnalu@gmail.com>
2021-09-10 16:57:54 -0700

    Adds auth dependencies and sessions

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const app = express();
 require('./config/db.connection');
+const app = express();
 const PORT = 4000;
 
 
-// SECTION Auth Modules
-// const session = require('express-session');
-// const MongoStore = require('connect-mongo');
+// SECTION Auth 
+const session = require('express-session');
+require('dotenv').config();
+const MongoStore = require('connect-mongo');
 
 
 // SECTION Internal Modules
@@ -16,19 +17,28 @@ const restaurantControllers = require('./controllers/restaurant_controllers');
 // SECTION Middleware 
 app.set('view engine', 'ejs');
 
+
 // Session controller
-// app.use(
-//   session({
-//     store: MongoStore.create()
-//   })
-// );
+app.use(
+  session({
+    store: MongoStore.create({ mongoUrl:process.env.MONGODB_URI }),
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7 * 2,
+    }
+  })
+);
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+
 // Custom middleware logger
 function logger(req,res,next) {
   console.log(`${req.url}: ${req.method} - ${new Date().toLocaleTimeString()}`);
+  console.log(req.session);
   next();
 };
 app.use(logger);
