1e8beeefbbdae22ab16a04f50700ad367f5079dc
lonnalu <lonnalu@gmail.com>
2021-09-13 09:41:13 -0700

    Adds register get and post route

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -3,18 +3,32 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { User } = require('../models');
 
+router.get('/register', function (req,res,next) {
+  return res.render('auth/register.ejs');
+})
+
 router.post('/register', async function (req,res) {
   try {
+    if (req.body.password !== req.body.passwordTwo) {
+      return res.send("Passwords do not match.");
+    };
+
     const foundUser = await User.exists({ 
       $or: [ {email: req.body.email}, {username: req.body.username} ],
     });
     if (foundUser) return res.redirect('/');
     
     const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hash;
+
+    const createdUser = await User.create(req.body);
+
+    return res.redirect('/');
 
   } catch (error) {
     console.log(error);
-    res.send(error);
+    return res.send(error);
   }
 });
 
