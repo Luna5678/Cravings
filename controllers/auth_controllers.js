ecafc9264425f9dbfd6866f1160cc8b44e9e9c09
lonnalu <lonnalu@gmail.com>
2021-09-13 11:41:14 -0700

    Adds styling to registration page. In progress

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -10,13 +10,21 @@ router.get('/register', function (req,res,next) {
 router.post('/register', async function (req,res) {
   try {
     if (req.body.password !== req.body.passwordTwo) {
-      return res.send("Passwords do not match.");
+      const context = {
+        error: "Passwords do not match.",
+      };
+      return res.render('auth/register.ejs', context);
     };
 
     const foundUser = await User.exists({ 
       $or: [ {email: req.body.email}, {username: req.body.username} ],
     });
-    if (foundUser) return res.redirect('/');
+    if (foundUser) {
+      const context = {
+        error: "Username or email already exists. Please log in."
+      }
+      return res.render('auth/register.ejs', context);
+    };
     
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(req.body.password, salt);
@@ -27,9 +35,20 @@ router.post('/register', async function (req,res) {
     return res.redirect('/');
 
   } catch (error) {
-    console.log(error);
-    return res.send(error);
+    const context = {
+      error: error,
+    };
+    return res.render('auth/register.ejs', context);
   }
 });
+
+router.get('/logout', async function(req,res,next) {
+  try {
+    await req.session.destroy();
+    return res.redirect('/login');
+  } catch (error) {
+    return res.send(error);
+  }
+})
 
 module.exports = router;
