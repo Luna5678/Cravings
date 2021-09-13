86357217d477720f6ebd4425a9966167b7f54fb0
lonnalu <lonnalu@gmail.com>
2021-09-12 22:37:49 -0700

    Cleans up review post controller and new review modal

--- controllers/review_controllers.js
+++ controllers/review_controllers.js
@@ -2,16 +2,25 @@ const express = require('express');
 const router = express.Router();
 const { Review, Restaurant } = require('../models')
 
-router.post('/', function (req,res, next) {
-  Review.create(req.body, function (error, createdReview) {
-    if (error) {
-      console.log(error);
-      req.error=error;
-      return next();
-    }
-
+router.post('/', async function (req,res, next) {
+  try {
+    const createdReview = await Review.create(req.body);
     return res.redirect(`/restaurants/${createdReview.restaurant}`);
-  })
-})
+  } catch (error) {
+    const context = {
+      error,
+    };
+    return next();
+  }
+  // Review.create(req.body, function (error, createdReview) {
+  //   if (error) {
+  //     console.log(error);
+  //     req.error=error;
+  //     return next();
+  //   }
+
+    
+  // })
+});
 
 module.exports = router;
