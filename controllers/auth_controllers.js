b100ca0eea90cd398e934279a0dc5dc67e05368c
lonnalu <lonnalu@gmail.com>
2021-09-13 23:09:36 -0700

    Adds update routes to account details

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -2,14 +2,21 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { User } = require('../models');
+const { removeListener } = require('../models/Restaurant');
+
+// Registration form
 
 router.get('/register', function (req,res,next) {
   return res.render('auth/register.ejs');
 })
 
+// Login form
+
 router.get('/login', function(req,res,next) {
   return res.render('auth/login.ejs');
 })
+
+// Registration verifications
 
 router.post('/register', async function (req,res) {
   try {
@@ -46,6 +53,8 @@ router.post('/register', async function (req,res) {
   }
 });
 
+// Login verifications
+
 router.post('/login', async function(req,res,next) {
   try {
     const foundUser = await User.findOne( { username: req.body.username });
@@ -75,6 +84,110 @@ router.post('/login', async function(req,res,next) {
     return res.send(error);
   };
 });
+
+// Populate Edit Profile Form
+
+router.get('/:id/edit', async function(req,res,next) {
+  // Redirects if user tries to edit a diff user's profile
+  if (req.session.currentUser.id !== req.params.id) {
+    return res.redirect(`/${req.session.currentUser.id}/edit`);
+  }
+  
+  try {
+    const foundUser = await User.findById(req.session.currentUser.id);
+    const context = {
+      user: foundUser,
+    }
+    return res.render('auth/edit.ejs', context)
+  } catch (error) {
+    console.log(error);
+        return res.send(error);
+  }
+})
+
+// Put Route for Updating Profile
+
+router.put('/profile/:id', async function(req,res,next) {
+  try {
+    const foundUser = await User.findById(req.session.currentUser.id);
+
+    const foundEmail = await User.exists({email: req.body.email});
+    if (foundEmail && (req.body.email !== foundUser.email)) {
+      const context = {
+        user: foundUser,
+        error: "New email already exists."
+      }
+      return res.render('auth/edit.ejs', context);
+    };
+
+    const foundUsername = await User.exists({username: req.body.username});
+    if (foundUsername && (req.body.username !== foundUser.username)) {
+      const context = {
+        user: foundUser,
+        error: "New username already exists."
+      }
+      return res.render('auth/edit.ejs', context);
+    };
+
+    // if (req.body.password !== req.body.passwordTwo) {
+    //   const context = {
+    //     error: "Passwords do not match.",
+    //   };
+    //   return res.render('auth/edit.ejs', context);
+    // };
+
+    // const salt = await bcrypt.genSalt(10);
+    // const hash = await bcrypt.hash(req.body.password, salt);
+    // req.body.password = hash;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.session.currentUser.id, 
+      { $set: {avatar: req.body.avatar,
+          username: req.body.username,
+          email: req.body.email,
+          bio: req.body.bio} }, 
+      { new: true }
+    );
+
+    return res.redirect(`/profile/${req.session.currentUser.id}`);
+  } catch (error) {
+    console.log(error);
+    return res.send(error);
+  }
+})
+
+// Put Route for Updating Password
+router.put('/pw/:id', async function(req,res,next) {
+  try {
+    const foundUser = await User.findById(req.session.currentUser.id);
+
+    if (req.body.password !== req.body.passwordTwo) {
+      const context = {
+        user: foundUser,
+        error: "Passwords do not match.",
+      };
+      return res.render('auth/edit.ejs', context);
+    };
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hash;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.session.currentUser.id, 
+      { $set: {password: hash} }, 
+      { new: true }
+    );
+
+    return res.redirect(`/profile/${req.session.currentUser.id}`);
+  } catch (error) {
+    console.log(error);
+    return res.send(error);
+  }
+  
+});
+
+// Logout 
 
 router.get('/logout', async function(req,res,next) {
   try {
--- controllers/profile_controllers.js
+++ controllers/profile_controllers.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { User, Review } = require('../models');
+const { User, Review, Restaurant } = require('../models');
 
 router.get('/:id', async function (req,res,next) {
   try {
     const foundUser = await User.findById( req.params.id );
-    const foundReviews = await Review.find({ user: req.params.id} ).sort('-createdAt')
+    const foundReviews = await Review.find({ user: req.params.id} ).sort('-createdAt');
+    const allRestaurants = await Restaurant.find({})
     const context = {
       profile: foundUser,
-      reviews: foundReviews
+      reviews: foundReviews,
+      restaurants: allRestaurants,
     };
     return res.render('profile/show.ejs', context)
   } catch (error) {
